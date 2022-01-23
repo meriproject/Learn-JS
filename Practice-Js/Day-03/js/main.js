8be0b3c212d8f377709e1b7995f6a8e78c6b80cf
meriproject <merischool1621@gmail.com>
2022-01-22 22:13:05 -0800

    take home c4

---
+++ Practice-Js/Day-03/js/main.js
@@ -0,0 +1,20 @@
+// Ask for the user's first name
+// Ask for the user's last name
+// Log the user's first name to the console
+// Alert the user's last name
+
+// *BONUS* 
+// Ask for the user's birthday
+// Ask the user to confirm their birthday input
+// Alert the user's birthday
+
+const fname= prompt("enter your fistname");
+const lname= prompt("enter your listname");
+console.log("first name is " + fname);
+alert("Last Name" + lname);
+
+///////////////////////
+
+const bDate=prompt("enter your birthdate");
+const bConfirm= confirm("please confirm your birthdate" + bDate);
+alert(bDate);
---
+++ Practice-Js/Day-04/Js/main.js
@@ -0,0 +1,6 @@
+// ------------ DAILY CHALLENGE ------------
+// Prompt the user for two different numbers
+// Convert the values to integers
+// Store the two integers as variables
+// Compare the two numbers
+// Alert the larger number
