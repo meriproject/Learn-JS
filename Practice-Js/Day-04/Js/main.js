266df0c995e5d776374b5a5280c823267ff4d72b
meriproject <merischool1621@gmail.com>
2022-01-22 22:56:08 -0800

    day 4 takehome

--- Practice-Js/Day-04/Js/main.js
+++ Practice-Js/Day-04/Js/main.js
@@ -4,3 +4,14 @@
 // Store the two integers as variables
 // Compare the two numbers
 // Alert the larger number
+
+let fNumber= prompt("enter the fist number");
+let sNumber= prompt("enter the second number");
+fNumber=parseInt(fNumber);
+sNumber=parseInt(sNumber);
+if(fNumber>sNumber){
+    alert("larger number is " + fNumber);
+}
+else{
+    alert("larger number is " + sNumber);
+}
